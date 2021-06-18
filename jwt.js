6a6588f032d3079c1b4c5272e00f95f875002f89
Joseph Benoy <josephbenoy03@gmail.com>
2021-06-19 01:03:51 +0530

    added validatetoken function to jwt

--- jwt.js
+++ jwt.js
@@ -6,6 +6,25 @@ const createToken = (payload)=>{
     return sign(payload,secret);
 }
 
+const validateToken = (req,res,next)=>{
+    const accessToken = req.cookie['access-token'];
+    if(!accessToken){
+        return res.status(400).json({error:"user not authenticated"});
+    }
+    else{
+        try{
+            const payload = verify(accessToken,secret);
+            if(payload){
+                req.authenticated = true;
+                req.username = payload.username;
+                return next();
+            }
+        }
+        catch(err){
+            res.status(400).json({error:err});
+        }
+    }
+}
 
 
 
