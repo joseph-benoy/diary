3a6be1ba3fa547663a45347a9aa64e0e54e06b39
Joseph Benoy <josephbenoy03@gmail.com>
2021-06-18 13:41:28 +0530

    added toggle password for login

--- client/src/components/loginForm/Loginform.js
+++ client/src/components/loginForm/Loginform.js
@@ -3,15 +3,24 @@ import './Loginform.scss';
 
 
 const Loginform = ()=>{
+    const togglePassword = ()=>{
+        const pass = document.getElementById('password');
+        if(pass.getAttribute('type')==='text'){
+            pass.setAttribute('type','password');
+        }
+        else{
+            pass.setAttribute('type','text');
+        }
+    }
     return(
-        <div className="container-fluid" style={{padding:"5vw"}}>
+        <div className="container-fluid main ">
         <form className="gy-2 gx-3 align-items-center">
             <div className="row">
             <div className="col-auto">
                 <label className="form-label" htmlFor="username">Username</label>
                 <div className="input-group">
                 <div className="input-group-text">@</div>
-                <input type="text" className="form-control" id="username" placeholder="Username" name="username"/>
+                <input type="text" className="form-control" id="username" placeholder="johndoe@example.com" name="username"/>
                 </div>
             </div>
             </div>
@@ -19,24 +28,24 @@ const Loginform = ()=>{
             <div className="col-auto">
                 <label className="form-label" htmlFor="password">Password</label>
                 <div className="input-group">
-                <div className="input-group-text" style={{color:"black"}}>&#128274;</div>
+                <div className="input-group-text"><i class="bi bi-shield-lock"></i></div>
                 <input type="password" className="form-control" id="password" placeholder="Password"/>
                 </div>
             </div>
             </div>
-            <div className="row">
+            <div className="row" style={{padding:"2vh 0vw"}}>
             <div className="col-auto">
                 <div className="form-check">
-                <input className="form-check-input" type="checkbox" id="passwordToggle" name="passwordToggle" />
-                <label className="form-check-label" htmlFor="passwordToggle">
+                <input onClick={togglePassword} className="form-check-input" type="checkbox" id="passwordToggle" name="passwordToggle" />
+                <label className="form-check-label"  htmlFor="passwordToggle">
                     Show password
                 </label>
                 </div>
             </div>
             </div>
             <div className="row">
-                <div className="d-grid gap-2">
-                    <button className="btn btn-primary" type="button">Sign In</button>
+                <div className="d-grid gap-2 col-12">
+                    <button className="btn btn-primary signin" type="button">Sign In</button>
                 </div>
             </div>
         </form>
