f56800cb7317c99c9d72a6f63cdc5e49973c076f
Joseph Benoy <josephbenoy03@gmail.com>
2021-06-18 19:16:50 +0530

    added not found to express index

--- index.js
+++ index.js
@@ -8,3 +8,34 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.all("*",(req,res)=>{
+    res.status(400).json({
+        error:"Invalid request"
+    });
+});
+app.listen(3001);
