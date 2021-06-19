ca588b8e296ba0bf06b02112a68e666bc5417a4e
Joseph Benoy <josephbenoy03@gmail.com>
2021-06-20 01:36:32 +0530

    added memories topbar

--- client/src/components/memories/Memories.js
+++ client/src/components/memories/Memories.js
@@ -1,6 +1,23 @@
+import "./Memories.scss";
+import AddMemoryModal from "../addMemoryModal/Addmemorymodal";
+
+
+
 const Memories = ()=>{
     return(
-        <h1>Memories</h1>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-lg-2">
+                    <h2>Memories</h2>
+                </div>
+                <div className="col-lg-8">
+                </div>
+                <div className="col-lg-2">
+                    <button className="addMemoryBtn" data-bs-toggle="modal" data-bs-target="#addMemoryModal"><i className="bi bi-bookmark-plus"></i></button>
+                    <AddMemoryModal/>
+                </div>
+            </div>
+        </div>
     );
 }
 
