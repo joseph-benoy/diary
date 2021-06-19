ee92202338acfba106b28a819f41be836fdf5e24
Joseph Benoy <josephbenoy03@gmail.com>
2021-06-19 13:09:26 +0530

    created sidebar ui

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -3,18 +3,16 @@ import './Sidebar.scss';
 
 const Sidebar = ()=>{
     return(
-        <div className="container-fluid sidebar">
+        <div className="col-lg-1 sidebar">
             <div className="row">
                 <Link to="/" className="logoLink"><i className="bi bi-journal-text"></i></Link>
             </div>
-            <div className="row">
-                <ul>
-                    <li><Link to="/dashboard"><i className="bi bi-house-door"></i></Link></li>
-                    <li><Link to="/createEntry"><i className="bi bi-file-earmark-plus"></i></Link></li>
-                    <li><Link to="/read"><i className="bi bi-book"></i></Link></li>
-                    <li><Link to="/memories"><i className="bi bi-bookmark-heart"></i></Link></li>
-                    <li><Link to="/settings"><i className="bi bi-gear"></i></Link></li>
-                </ul>
+            <div className="row navlinks">
+                <Link to="/dashboard"><i className="bi bi-house-door"></i></Link>
+                <Link to="/createEntry"><i className="bi bi-file-earmark-plus"></i></Link>
+                <Link to="/read"><i className="bi bi-book"></i></Link>
+                <Link to="/memories"><i className="bi bi-bookmark-heart"></i></Link>
+                <Link to="/settings" id="settingsLink"><i className="bi bi-gear"></i></Link>
             </div>
         </div>
     );
