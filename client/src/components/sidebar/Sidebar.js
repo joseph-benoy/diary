83b8ab03542e6ea8009a5dd7576249810b41daf7
Joseph Benoy <josephbenoy03@gmail.com>
2021-06-19 19:25:23 +0530

    added highligting for the sidebar

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -1,8 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import './Sidebar.scss';
+import { useState,useEffect } from 'react';
 
 
 const Sidebar = ()=>{
+    const [active,setActive] = useState();
+    const {page} = useParams();
+    console.log(page);
+    useEffect(()=>{
+        document.getElementById(page).style = "color:#7952b3;background-color:white";
+    });
     return(
         <div className="col-lg-1 sidebar">
             <div className="container-fluid" style={{padding:0}}>
@@ -10,11 +17,11 @@ const Sidebar = ()=>{
                     <Link to="/dashboard/home" className="logoLink"><i className="bi bi-journal-text"></i></Link>
                 </div>
                 <div className="row navlinks">
-                    <Link to="/dashboard/home"><i className="bi bi-house-door"></i></Link>
-                    <Link to="/dashboard/createentry"><i className="bi bi-file-earmark-plus"></i></Link>
-                    <Link to="/dashboard/read"><i className="bi bi-book"></i></Link>
-                    <Link to="/dashboard/memories"><i className="bi bi-bookmark-heart"></i></Link>
-                    <Link to="/dashboard/settings"><i className="bi bi-gear"></i></Link>
+                    <Link to="/dashboard/home" id="home"><i className="bi bi-house-door"></i></Link>
+                    <Link to="/dashboard/createentry" id="createentry"><i className="bi bi-file-earmark-plus"></i></Link>
+                    <Link to="/dashboard/read" id="read"><i className="bi bi-book"></i></Link>
+                    <Link to="/dashboard/memories" id="memories"><i className="bi bi-bookmark-heart"></i></Link>
+                    <Link to="/dashboard/settings" id="settings"><i className="bi bi-gear"></i></Link>
                     <Link to="/"><i className="bi bi-box-arrow-left"></i></Link>
                 </div>
             </div>
