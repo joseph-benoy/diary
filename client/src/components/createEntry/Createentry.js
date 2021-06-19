e51a837e46b6f9650890937fc9c7ce2f5bad645b
Joseph Benoy <josephbenoy03@gmail.com>
2021-06-19 23:57:42 +0530

    added quill editor

--- client/src/components/createEntry/Createentry.js
+++ client/src/components/createEntry/Createentry.js
@@ -1,6 +1,44 @@
+import React, { useState,useMemo, createElement } from "react";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+
+
 const CreateEntry = ()=>{
+    const selectLocalImage = ()=>{
+        let input = document.createElement("input");
+        input.setAttribute('type','file');
+        input.click();
+        input.onchange = async ()=>{
+            
+        }
+    }
+    const modules = useMemo(() => ({
+        toolbar: {
+          container: [
+            [{ header: [1, 2, false] }],
+            ['bold', 'italic', 'underline'],
+            [{ list: 'ordered' }, { list: 'bullet' }],
+            ['image', 'code-block']
+          ],
+          handlers: {
+            image: selectLocalImage
+          }
+        }
+      }), []);
+    const [value, setValue] = useState('');
+
     return(
-        <h1>Create entry</h1>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-lg-3">
+                    <h2>Today's entry</h2>
+                </div>
+            </div>
+            <div className="row">
+                <ReactQuill placeholder="Enter something...." theme="snow" value={value}   modules={modules} onChange={setValue}/>
+            </div>
+        </div>
     );
 }
 
