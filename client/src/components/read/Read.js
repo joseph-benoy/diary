4b2a6be25d922473a79b8d4db76f3bd7228fc070
Joseph Benoy <josephbenoy03@gmail.com>
2021-06-22 15:25:45 +0530

    added read entry page topbar

--- client/src/components/read/Read.js
+++ client/src/components/read/Read.js
@@ -1,6 +1,27 @@
+import "./Read.scss";
+import Calendar from 'react-calendar';
+import { useState } from "react";
+
 const Read = ()=>{
     return(
-        <h1>Read</h1>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-lg-3">
+                    <h1>Read diary</h1>
+                </div>
+                <div className="col-lg-7">
+                    <input type="date" className="form-control" max={new Date().toISOString().split("T")[0]} />
+                </div>
+                <div className="col-lg-2">
+                    <button className="saveBtn">Read</button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-12">
+                    {}
+                </div>
+            </div>
+        </div>
     );
 }
 
